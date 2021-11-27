77ed5f7c050de9aaae373abcd4589785effddb2f
Santiago Luis Ocón <ocon.santiago@gmail.com>
2021-11-27 11:58:59 -0300

    Modificado GestorPalabrasClaves.

--- src/interfaces/IGestorPalabrasClaves.java
+++ src/interfaces/IGestorPalabrasClaves.java
@@ -16,8 +16,12 @@ public interface IGestorPalabrasClaves {
     public static final String MSJ_OK = "Palabra Clave creada con éxito.";
     public static final String MSJ_REP = "Ya existe esta palabra clave.";
     public static final String MSJ_ERROR = "Error.";    
+    public static final String MSJ_OK_BORRAR = "Palabra Clave borrada con éxito.";
     
     public String nuevaPalabraClave(String nombre);
+    public String borrarPalabraClave(PalabraClave palabraClave);
+    public ArrayList<PalabraClave> buscarPalabrasClaves(String nombre);
     public ArrayList<PalabraClave> verPalabrasClaves();
     public PalabraClave verPalabraClave(String nombre);
+    public boolean existeEstaPalabraClave(PalabraClave palabraClave);
 }
--- src/palabrasclaves/modelos/GestorPalabrasClaves.java
+++ src/palabrasclaves/modelos/GestorPalabrasClaves.java
@@ -6,8 +6,11 @@
 package palabrasclaves.modelos;
 
 import interfaces.IGestorPalabrasClaves;
+import interfaces.IGestorPublicaciones;
 import java.util.ArrayList;
 import java.util.Comparator;
+import publicaciones.modelos.GestorPublicaciones;
+import publicaciones.modelos.Publicacion;
 
 /**
  *
@@ -16,6 +19,7 @@ import java.util.Comparator;
 public class GestorPalabrasClaves implements IGestorPalabrasClaves {
     public static GestorPalabrasClaves gestor;    
     private ArrayList<PalabraClave> palabrasClaves = new ArrayList<>();
+    IGestorPublicaciones gestorPublicaciones = GestorPublicaciones.crear();
     Comparator<PalabraClave> comparadorPC=(PalabraClave pcA, PalabraClave pcB) -> pcA.verNombre().compareTo(pcB.verNombre());    
     
     private GestorPalabrasClaves() {
@@ -59,5 +63,50 @@ public class GestorPalabrasClaves implements IGestorPalabrasClaves {
         else {
             return palabrasClaves.get(index);
         }
+    }
+
+    @Override
+    public String borrarPalabraClave(PalabraClave palabraClave) {
+        if((palabraClave!=null) && (!palabraClave.verNombre().isBlank())){
+            for(Publicacion p: gestorPublicaciones.verPublicaciones()) {
+                if(palabraClave.equals(p.verPalabrasClave()))
+                    return MSJ_REP;
+            }
+            if(this.existeEstaPalabraClave(palabraClave)) {
+                this.palabrasClaves.remove(palabraClave);
+                return MSJ_OK_BORRAR;
+            }                
+            else
+                return MSJ_ERROR;
+        }                  
+        else 
+            return MSJ_ERROR;
+    }
+
+    @Override
+    public ArrayList<PalabraClave> buscarPalabrasClaves(String nombre) {
+        ArrayList<PalabraClave> palabrasClavesBuscadas= new ArrayList<>();
+        if((nombre!= null) && (!nombre.isBlank())){
+            for(PalabraClave p: palabrasClaves){
+                if((p.verNombre().equals(nombre)) || (p.verNombre().startsWith(nombre))){
+                    if((!palabrasClavesBuscadas.contains(p)))
+                        palabrasClavesBuscadas.add(p);
+                } 
+            }
+            if(palabrasClavesBuscadas != null){
+                palabrasClavesBuscadas.sort(comparadorPC);
+                return palabrasClavesBuscadas;
+            }      
+        }
+        return palabrasClavesBuscadas;
+    }
+
+    @Override
+    public boolean existeEstaPalabraClave(PalabraClave palabraClave) {
+        for(PalabraClave pc: this.palabrasClaves){
+            if(pc.equals(palabraClave))
+                return true;
+        }
+        return false;
     }
 }
